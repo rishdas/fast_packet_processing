aee1b608efb07fd7396395225af7cdef2704e802
rishdas <sirshakdas@gmail.com>
2016-04-02 04:56:55 -0400

    wrongly deleted

---
+++ include/fpp_common.h
@@ -0,0 +1,48 @@
+/*Includes*/
+#include<stdio.h>
+#include<stdint.h>
+#include<unistd.h>
+#include<string.h>
+#include<assert.h>
+#include<stdlib.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+
+/*Hash Defines*/
+#define bool_t char
+#define TRUE (bool_t)1
+#define FALSE (bool_t)0
+
+/*Structures*/
+typedef struct routing_tab_entry_
+{
+    in_addr_t next_hop;
+    in_addr_t route;
+    uint32_t  prefix;
+}routing_tab_entry_t;
+
+typedef struct routing_table_
+{
+    routing_tab_entry_t *routingtab;
+    uint32_t count;
+}routing_tab_t;
+
+typedef struct fpp_config_
+{
+    FILE   *r_table;
+    bool_t debug;
+    routing_tab_t rout_tab;
+}fpp_config_t;
+
+
+/*Global Variables*/
+fpp_config_t fpp_config;
+
+/*Function Signatures*/
+
+int fpp_test_parse_arguments(int argc, char **argv);
+int fpp_test_parse_routing_table();
+int fpp_test_conduct_tests();
+
+
